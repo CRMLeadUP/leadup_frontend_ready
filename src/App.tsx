dfc3dcd7c623f80a161426b1f44829dd37a1208d
CRMLeadUP <juniorzinho11@gmail.com>
2025-05-09 13:03:47 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1 style={{ color: "#1d4ed8" }}>🚀 LeadUP CRM está funcionando!</h1>
+    </div>
+  );
+}
